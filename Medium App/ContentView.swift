f2fce3b0cc7fd8b048d809f2f1bb71eac187be78
muhammad mufti ramdhani <muftiramdhani25@gmail.com>
2021-02-20 10:25:32 +0700

    Initial Commit

---
+++ Medium App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Medium App
+//
+//  Created by abdullah irianda on 20/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
